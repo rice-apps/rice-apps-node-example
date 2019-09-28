ed6afaec5bfb7545467bb14b6a54776aa01aceb0
jlt10 <jamietan221@gmail.com>
2019-09-28 13:32:24 -0500

    Finished get requests

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
 # Example Node.js Server with Express
 
-To run, navigate to the project directory in terminal and  type `npm start`
+To run, navigate to the project directory in terminal and  type `npm start` or `node server.js`.
 
 ## Routes
-1. '/' (GET) - string 'Hello world' 
+1. '/' (GET) - response is simple "hello world" string
     * Params: None
 
-2. '/' (GET) -  
-    * Params: None
+2. '/team/:name' (GET) - response is a string that changes whether or not a team member is making the request
+    * Params: name - first name of a team member
 
-3. '/' (GET) -  
+3. '/secret' (GET) -  response is a secret image ;)
     * Params: None
 
 4. '/' (PUT) -  
     * Params: 
-    
+
 5. '/' (POST) -  
     * Params: 
--- server.js
+++ server.js
@@ -2,6 +2,30 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// Simple default get request with no parameters
+app.get('/', (req, res) => res.send(
+    'Hello World! This server is SO GOOD you guys.' +
+    ' Unlike SOME other servers, this one won\'t kick you ' + 
+    'out of Applebees for eating the wax fruit.')
+)
 
+// Names of the members of the best team
+const teamNames = ['jamie', 'will', 'skylar', 'peter', 'max', 'david', 'manan']
+// Get request that will respond more enthusiastically if you are on the team.
+app.get('/team/:name', function (req, res) {
+    const name = req.params.name;
+    if (teamNames.includes(name.toLowerCase())) {
+        res.send("Nice to see you, team member " + name + " :D")
+    } else {
+        res.send("Thanks for visiting " + name + " :)")
+    }
+})
+
+// Get request that responds with a secret image
+app.get('/secret', function (req, res) {
+    console.log("This could be you!")
+    res.sendFile('/secret.jpg', { root: '.' })
+});
+
+// Starts the server.
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
