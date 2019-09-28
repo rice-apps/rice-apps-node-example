6e0a098c94aaadbd244440d7c6d06b539942c5c5
jlt10 <jamietan221@gmail.com>
2019-09-28 14:04:38 -0500

    Finished counter example

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 To run, navigate to the project directory in terminal and  type `npm start` or `node server.js`.
 
 ## Routes
-1. '/' (GET) - response is simple "hello world" string
+1. '/' (GET) - response is simple "hello world" home page
     * Params: None
 
 2. '/team/:name' (GET) - response is a string that changes whether or not a team member is making the request
@@ -11,9 +11,6 @@ To run, navigate to the project directory in terminal and  type `npm start` or `
 
 3. '/secret' (GET) -  response is a secret image ;)
     * Params: None
-
-4. '/' (PUT) -  
-    * Params: 
 
 5. '/' (POST) -  
     * Params: 
--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-// Simple default get request with no parameters
-app.get('/', (req, res) => res.send(
-    'Hello World! This server is SO GOOD you guys.' +
-    ' Unlike SOME other servers, this one won\'t kick you ' + 
-    'out of Applebees for eating the wax fruit.')
+// Home page of the website
+const homepage = `
+<h1>Hello World</h1> 
+<p>This server is SO GOOD you guys. Unlike SOME other servers, 
+this one won't kick you out of Applebees for eating the wax fruit.</p>
+`
+
+// Get request that displays the home page
+app.get('/', (req, res) => res.send(homepage)
 )
 
 // Names of the members of the best team
@@ -26,6 +30,22 @@ app.get('/secret', function (req, res) {
     console.log("This could be you!")
     res.sendFile('/secret.jpg', { root: '.' })
 });
+
+// Counting button
+const count_button = `
+<form action="count" method="post">
+    <button name="inc" value="1">Count</button>
+</form>
+`
+// Number of times this page has been requested.
+let count = 0;
+app.get('/count', (req, res) => {
+    res.send(count_button + "<p>" + count + "</p>");
+})
+app.post('/count', (req, res) => {
+    count += 1;
+    res.send(count_button + "<p>" + count + "</p>");
+})
 
 // Starts the server.
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
